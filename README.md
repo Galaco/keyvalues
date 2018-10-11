811c3f016891a0e0aee859cc2ea5bbac0a1f22e2
Galaco <mail@galaco.me>
2018-10-11 19:14:30 +0100

    Basic parsing
    Fails to

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# gameinfo
-Source engine gameinfo.txt parser
+# vmf
+Golang library for manipulating hammer Editor .vmf map files
+
+This library constructs a simple node tree that you can query any structure(s) and any property(s) of.
+
+### Usage
+```golang
+package main
+
+import (
+    "fmt"
+    "github.com/galaco/vmf"
+)
+
+func main() {
+	file,_ := os.Open("de_dust2.vmf")
+
+	reader := vmf.NewReader(file)
+	f,_ := reader.Read()
+
+	fmt.Println(f.Entities.GetChildrenByKey("entity")[0].GetProperty("classname"))
+	fmt.Println(f.Entities.GetChildrenByKey("entity")[0].GetChildrenByKey("solid"))
+}
+```
---
+++ reader.go
@@ -0,0 +1,112 @@
+package keyvalues
+
+import (
+	"strings"
+	"bufio"
+	"io"
+)
+
+const CHAR_ENTER_SCOPE = "{"
+const CHAR_EXIT_SCOPE = "}"
+const CHAR_ESCAPE = "\""
+const CHAR_DISCARD_CUTSET = "\t \r\n"
+const CHAR_SEPARATOR = " "
+const CHAR_TAB = "\t"
+const CHAR_COMMENT = "//"
+const NODE_KEY_ROOT = "$root"
+
+type Reader struct {
+	file io.Reader
+}
+
+// Return a new Vmf Reader
+func NewReader(file io.Reader) Reader {
+	reader := Reader{}
+	reader.file = file
+	return reader
+}
+
+// Read buffer file into our defined structures
+// Returns a fully mapped Vmf structure
+func (reader *Reader) Read() (keyvalue KeyValue, err error) {
+	bufReader := bufio.NewReader(reader.file)
+
+	rootNode := KeyValue{
+		key: NODE_KEY_ROOT,
+	}
+
+	readScope(bufReader, &rootNode)
+
+	return rootNode,err
+}
+
+// Read a single scope
+// Constructs a KeyValue node tree for a single scope
+// Recursively parses all child scopes too
+// Param: scope is the current scope to write to
+func readScope(reader *bufio.Reader, scope *KeyValue) *KeyValue {
+	for {
+		line,err := reader.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+
+		// Remove any comments
+		line = strings.Split(line, CHAR_COMMENT)[0]
+		// trim padding
+		line = strings.Trim(line, CHAR_DISCARD_CUTSET)
+		// Simplify parsing the line
+		line = strings.Replace(line, CHAR_TAB, CHAR_SEPARATOR, -1)
+
+		if len(line) == 0 {
+			continue
+		}
+
+
+		// New scopez
+		if strings.Contains(line, CHAR_ENTER_SCOPE) {
+			// Scope is opened when the key is read
+			// There may be situations where there is no key, so we must account for that
+			subScope := scope.value[len(scope.value) - 1].(KeyValue)
+			scope.value = append(scope.value[:len(scope.value)-1], *readScope(reader, &subScope))
+			continue
+		}
+
+		// Exit scope
+		if strings.Contains(line, CHAR_EXIT_SCOPE) {
+			break
+		}
+
+		// Read scope
+		prop := strings.Split(line, CHAR_SEPARATOR)
+
+		// Only the key is defined here
+		// This *SHOULD* mean key has children
+		if len(prop) == 1 {
+			//Create new scope
+			kv := KeyValue{
+				key: strings.Replace(prop[0], CHAR_ESCAPE, "", -1),
+			}
+
+			scope.value = append(scope.value, kv)
+			continue
+		}
+
+
+		// Read keyvalue & append to current scope
+		scope.value = append(scope.value, parseKV(line))
+	}
+
+	return scope
+}
+
+func parseKV(line string) KeyValue {
+	prop := strings.Split(line, CHAR_SEPARATOR)
+	// value also defined on this line
+	val := strings.Replace(line, prop[0] + CHAR_SEPARATOR, "", -1)
+
+	return KeyValue{
+		key: prop[0],
+		value: append(make([]interface{}, 0), strings.Replace(val, CHAR_ESCAPE, "", -1)),
+	}
+}
